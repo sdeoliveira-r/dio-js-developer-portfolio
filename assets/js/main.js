79f2ed88ff1b40ebae91bf4e02d9c0d40d890240
sdeoliveira-r <sdeoliveira.r@gmail.com>
2023-03-01 18:18:01 -0300

    update profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,33 @@
 //Gerir HTML
 
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('.profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    phone.innerText =  profileData.email
+    email.href = `mailto:${profileData.email}`
+    
+
+}
+
 (async () => {
     const profileData = await fetchPrifileDate()
-    console.log(profileData)
+    updateProfileInfo(profileData)
+    
 
 })()
