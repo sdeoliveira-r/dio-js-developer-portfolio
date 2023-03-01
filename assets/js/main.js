528a31825031a97fa505925746a57b53f566be85
sdeoliveira-r <sdeoliveira.r@gmail.com>
2023-03-01 18:51:25 -0300

    update hardskills

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,9 +25,15 @@ function updateProfileInfo(profileData) {
 
 }
 
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
 (async () => {
     const profileData = await fetchPrifileDate()
     updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
     
 
 })()
