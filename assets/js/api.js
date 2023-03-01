bf9c71d1e245aaac85905f8993c3f6ec167ee44b
sdeoliveira-r <sdeoliveira.r@gmail.com>
2023-03-01 17:57:03 -0300

    Update profile

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,8 @@
 async function fetchPrifileDate() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/sdeoliveira-r/portfolio/master/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
+
+
+//https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json
