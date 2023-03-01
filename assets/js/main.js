af202ec800c48a5c4537c41afc9075ddced9a093
sdeoliveira-r <sdeoliveira.r@gmail.com>
2023-03-01 20:03:47 -0300

    update experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,12 +41,25 @@ function updateLanguages(profileData) {
     languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
 }
 
+function updatePortifolio (profileData ) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+            <li>
+                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+                <a href="${project.url}" target="_blank">${project.url}</a>
+            </li>
+        `
+    }).join('')
+}
+
 (async () => {
     const profileData = await fetchPrifileDate()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
     updateLanguages(profileData)
+    updatePortifolio(profileData)
     
 
 })()
